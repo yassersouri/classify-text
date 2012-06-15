caead864d82fe66b092f3d56fd1a66bd7ebde38c
Yasser Souri <yassersouri@gmail.com>
2012-06-15 22:30:16 +0430

    add a function to calculate BOW representation

--- main.py
+++ main.py
@@ -50,34 +50,57 @@ def main():
 
 def test_main():
 	directory = 'ds2'
+	# load the dataset from disk
 	files = sklearn.datasets.load_files(directory)
-	refine_all_emails(files.data)
-	print files.data[0]
 
-"""
-Does `refine_single_email` for every single email included in the list
-parameter is a list of strings
-returns NOTHING!
-"""
+	# refine them
+	refine_all_emails(files.data)
+
+	# calculate the BOW representation
+	word_counts = bagOfWords(files.data)
+
+	#cross validation
+	
+
+def bagOfWords(files_data):
+	"""
+	Converts a list of strings (which are loaded from files) to a BOW representation of it
+	parameter 'files_data' is a list of strings
+	returns a `scipy.sparse.coo_matrix` 
+	"""
+
+	count_vector = sklearn.feature_extraction.text.CountVectorizer()
+	return count_vector.fit_transform(files.data)
+
 def refine_all_emails(file_data):
+	"""
+	Does `refine_single_email` for every single email included in the list
+	parameter is a list of strings
+	returns NOTHING!
+	"""
+
 	for i, email in zip(range(len(file_data)),file_data):
 		file_data[i] = refine_single_email(email)
 
-"""
-Delete the unnecessary information in the header of emails
-Deletes only lines in the email that starts with 'Path:', 'Newsgroups:', 'Xref:'
-parameter is a string.
-returns a string.
-"""
 def refine_single_email(email):	
+	"""
+	Delete the unnecessary information in the header of emails
+	Deletes only lines in the email that starts with 'Path:', 'Newsgroups:', 'Xref:'
+	parameter is a string.
+	returns a string.
+	"""
+
 	parts = email.split('\n')
 	newparts = []
 
+	# finished is when we have reached a line with something like 'Lines:' at the begining of it
+	# this is because we want to only remove stuff from headers of emails
+	# look at the dataset!
 	finished = False
 	for part in parts:
 		if finished:
 			newparts.append(part)
-			pass
+			continue
 		if not (part.startswith('Path:') or part.startswith('Newsgroups:') or part.startswith('Xref:')) and not finished:
 			newparts.append(part)
 		if part.startswith('Lines:'):
