27909c81131d4dddf5154d13f2e09dbb85c26629
yassersouri <yassersouri@gmail.com>
2016-11-30 12:41:39 +0330

    pep 8 fixes

--- main.py
+++ main.py
@@ -11,125 +11,128 @@ import sys
 import os
 import glob
 
+
 def main():
-	init()
+    init()
 
-	# get the dataset 
-	print colored("Where is the dataset?", 'cyan', attrs=['bold'])
-	print colored('warning: files might get deleted if they are incompatible with utf8', 'yellow')
-	ans = sys.stdin.readline()
-	# remove any newlines or spaces at the end of the input
-	path = ans.strip('\n')
-	if path.endswith(' '):
-		path = path.rstrip(' ')
+    # get the dataset
+    print colored("Where is the dataset?", 'cyan', attrs=['bold'])
+    print colored('warning: files might get deleted if they are incompatible with utf8', 'yellow')
+    ans = sys.stdin.readline()
+    # remove any newlines or spaces at the end of the input
+    path = ans.strip('\n')
+    if path.endswith(' '):
+        path = path.rstrip(' ')
 
-	# preprocess data into two folders instead of 6
-	print colored("Reorganizing folders, into two classes", 'cyan', attrs=['bold'])
-	reorganize_dataset(path)
+    # preprocess data into two folders instead of 6
+    print colored("Reorganizing folders, into two classes", 'cyan', attrs=['bold'])
+    reorganize_dataset(path)
 
+    print '\n\n'
 
-	print '\n\n'
+    # do the main test
+    main_test(path)
 
-	# do the main test
-	main_test(path)
 
 def reorganize_dataset(path):
-	likes = ['rec.sport.hockey', 'sci.crypt', 'sci.electronics']
-	dislikes = ['sci.space', 'rec.motorcycles', 'misc.forsale']
+    likes = ['rec.sport.hockey', 'sci.crypt', 'sci.electronics']
+    dislikes = ['sci.space', 'rec.motorcycles', 'misc.forsale']
 
-	folders = glob.glob(path + '/*')
-	if len(folders) == 2:
-		return
-	else:
-		# create `likes` and `dislikes` directories
-		if not os.path.exists(path + '/' + 'likes'):
-			os.makedirs(path + '/' + 'likes')
-		if not os.path.exists(path + '/' + 'dislikes'):
-			os.makedirs(path + '/' + 'dislikes')
+    folders = glob.glob(path + '/*')
+    if len(folders) == 2:
+        return
+    else:
+        # create `likes` and `dislikes` directories
+        if not os.path.exists(path + '/' + 'likes'):
+            os.makedirs(path + '/' + 'likes')
+        if not os.path.exists(path + '/' + 'dislikes'):
+            os.makedirs(path + '/' + 'dislikes')
 
-		for like in likes:
-			files = glob.glob(path + '/' + like + '/*')
-			for f in files:
-				parts = f.split('/')
-				name = parts[len(parts) -1]
-				newname = like + '_' + name
-				os.rename(f, path+'/likes/'+newname)
-			os.rmdir(path + '/' + like)
-		
-		for like in dislikes:
-			files = glob.glob(path + '/' + like + '/*')
-			for f in files:
-				parts = f.split('/')
-				name = parts[len(parts) -1]
-				newname = like + '_' + name
-				os.rename(f, path+'/dislikes/'+newname)
-			os.rmdir(path + '/' + like)
+        for like in likes:
+            files = glob.glob(path + '/' + like + '/*')
+            for f in files:
+                parts = f.split('/')
+                name = parts[len(parts) - 1]
+                newname = like + '_' + name
+                os.rename(f, path + '/likes/' + newname)
+            os.rmdir(path + '/' + like)
 
-def main_test(path = None):
-	dir_path = path or 'dataset'
-
-	remove_incompatible_files(dir_path)
-
-	print '\n\n'
-
-	# load data
-	print colored('Loading files into memory', 'green', attrs=['bold'])
-	files = sklearn.datasets.load_files(dir_path)
-
-	# refine all emails
-	print colored('Refining all files', 'green', attrs=['bold'])
-	util.refine_all_emails(files.data)
-
-	# calculate the BOW representation
-	print colored('Calculating BOW', 'green', attrs=['bold'])
-	word_counts = util.bagOfWords(files.data)
-
-	# TFIDF
-	print colored('Calculating TFIDF', 'green', attrs=['bold'])
-	tf_transformer = sklearn.feature_extraction.text.TfidfTransformer(use_idf=True).fit(word_counts)
-	X = tf_transformer.transform(word_counts)
+        for like in dislikes:
+            files = glob.glob(path + '/' + like + '/*')
+            for f in files:
+                parts = f.split('/')
+                name = parts[len(parts) - 1]
+                newname = like + '_' + name
+                os.rename(f, path + '/dislikes/' + newname)
+            os.rmdir(path + '/' + like)
 
 
-	print '\n\n'
+def main_test(path=None):
+    dir_path = path or 'dataset'
 
-	# create classifier
-	# clf = sklearn.naive_bayes.MultinomialNB()
-	# clf = sklearn.svm.LinearSVC()
-	n_neighbors = 11
-	weights = 'uniform'
-	weights = 'distance'
-	clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    remove_incompatible_files(dir_path)
 
-	# test the classifier
-	print '\n\n'
-	print colored('Testing classifier with train-test split', 'magenta', attrs=['bold'])
-	test_classifier(X, files.target, clf, test_size=0.2, y_names=files.target_names, confusion=False)
+    print '\n\n'
+
+    # load data
+    print colored('Loading files into memory', 'green', attrs=['bold'])
+    files = sklearn.datasets.load_files(dir_path)
+
+    # refine all emails
+    print colored('Refining all files', 'green', attrs=['bold'])
+    util.refine_all_emails(files.data)
+
+    # calculate the BOW representation
+    print colored('Calculating BOW', 'green', attrs=['bold'])
+    word_counts = util.bagOfWords(files.data)
+
+    # TFIDF
+    print colored('Calculating TFIDF', 'green', attrs=['bold'])
+    tf_transformer = sklearn.feature_extraction.text.TfidfTransformer(use_idf=True).fit(word_counts)
+    X = tf_transformer.transform(word_counts)
+
+    print '\n\n'
+
+    # create classifier
+    # clf = sklearn.naive_bayes.MultinomialNB()
+    # clf = sklearn.svm.LinearSVC()
+    n_neighbors = 11
+    weights = 'uniform'
+    weights = 'distance'
+    clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+
+    # test the classifier
+    print '\n\n'
+    print colored('Testing classifier with train-test split', 'magenta', attrs=['bold'])
+    test_classifier(X, files.target, clf, test_size=0.2, y_names=files.target_names, confusion=False)
+
 
 def remove_incompatible_files(dir_path):
-	# find incompatible files
-	print colored('Finding files incompatible with utf8: ', 'green', attrs=['bold'])
-	incompatible_files = util.find_incompatible_files(dir_path)
-	print colored(len(incompatible_files), 'yellow'), 'files found'
+    # find incompatible files
+    print colored('Finding files incompatible with utf8: ', 'green', attrs=['bold'])
+    incompatible_files = util.find_incompatible_files(dir_path)
+    print colored(len(incompatible_files), 'yellow'), 'files found'
 
-	# delete them
-	if(len(incompatible_files) > 0):
-		print colored('Deleting incompatible files', 'red', attrs=['bold'])
-		util.delete_incompatible_files(incompatible_files)
+    # delete them
+    if(len(incompatible_files) > 0):
+        print colored('Deleting incompatible files', 'red', attrs=['bold'])
+        util.delete_incompatible_files(incompatible_files)
+
 
 def test_classifier(X, y, clf, test_size=0.4, y_names=None, confusion=False):
-	#train-test split
-	print 'test size is: %2.0f%%' % (test_size*100)
-	X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(X, y, test_size=test_size)
+    # train-test split
+    print 'test size is: %2.0f%%' % (test_size * 100)
+    X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(X, y, test_size=test_size)
 
-	clf.fit(X_train, y_train)
-	y_predicted = clf.predict(X_test)
+    clf.fit(X_train, y_train)
+    y_predicted = clf.predict(X_test)
 
-	if not confusion:
-		print colored('Classification report:', 'magenta', attrs=['bold'])
-		print sklearn.metrics.classification_report(y_test, y_predicted, target_names=y_names)
-	else:
-		print colored('Confusion Matrix:', 'magenta', attrs=['bold'])
-		print sklearn.metrics.confusion_matrix(y_test, y_predicted)
+    if not confusion:
+        print colored('Classification report:', 'magenta', attrs=['bold'])
+        print sklearn.metrics.classification_report(y_test, y_predicted, target_names=y_names)
+    else:
+        print colored('Confusion Matrix:', 'magenta', attrs=['bold'])
+        print sklearn.metrics.confusion_matrix(y_test, y_predicted)
 
 if __name__ == '__main__':
-	main()
+    main()
--- util.py
+++ util.py
@@ -6,143 +6,150 @@ import sklearn.naive_bayes
 import sklearn.cross_validation
 import sklearn.svm
 import sklearn.neighbors
-def main():
-	from colorama import init
-	from termcolor import colored
-	init()
 
-	test_main()
-	
+
+def main():
+    from colorama import init
+    from termcolor import colored
+    init()
+
+    test_main()
 
 
 def test_main():
-	directory = 'ds2'
-	directory = 'dataset'
-	directory = 'ds3'
-	# load the dataset from disk
-	files = sklearn.datasets.load_files(directory)
+    directory = 'ds2'
+    directory = 'dataset'
+    directory = 'ds3'
+    # load the dataset from disk
+    files = sklearn.datasets.load_files(directory)
 
-	# refine them
-	refine_all_emails(files.data)
+    # refine them
+    refine_all_emails(files.data)
 
-	# calculate the BOW representation
-	word_counts = bagOfWords(files.data)
+    # calculate the BOW representation
+    word_counts = bagOfWords(files.data)
 
-	# TFIDF
-	tf_transformer = sklearn.feature_extraction.text.TfidfTransformer(use_idf=True).fit(word_counts)
-	X_tfidf = tf_transformer.transform(word_counts)
+    # TFIDF
+    tf_transformer = sklearn.feature_extraction.text.TfidfTransformer(use_idf=True).fit(word_counts)
+    X_tfidf = tf_transformer.transform(word_counts)
 
+    X = X_tfidf
 
-	X = X_tfidf
+    # cross validation
+    # clf = sklearn.naive_bayes.MultinomialNB()
+    # clf = sklearn.svm.LinearSVC()
+    n_neighbors = 5
+    weights = 'uniform'
+    # weights = 'distance'
+    clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    scores = cross_validation(X, files.target, clf, cv=5)
+    pretty_print_scores(scores)
 
-	#cross validation
-	# clf = sklearn.naive_bayes.MultinomialNB()
-	# clf = sklearn.svm.LinearSVC()
-	n_neighbors = 5
-	weights = 'uniform'
-	# weights = 'distance'
-	clf = sklearn.neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
-	scores = cross_validation(X, files.target, clf, cv=5)
-	pretty_print_scores(scores)
 
 def delete_incompatible_files(files):
-	"""
-	Deletes the list of files that are passed to it from file system!
-	argument `files` is a list of strings. containing absolute or relative pathes
-	"""
-	import os
-	for f in files:
-		print colored("deleting file:", 'red'), f
-		os.remove(f)
+    """
+    Deletes the list of files that are passed to it from file system!
+    argument `files` is a list of strings. containing absolute or relative pathes
+    """
+    import os
+    for f in files:
+        print colored("deleting file:", 'red'), f
+        os.remove(f)
+
 
 def find_incompatible_files(path):
-	"""
-	Finds the filenames that are incompatible with `CountVectorizer`. These files are usually not compatible with UTF8!
-	parameter `path` is the absolute or relative path of the training data's root directory.
-	returns a list of strings.
-	"""
+    """
+    Finds the filenames that are incompatible with `CountVectorizer`. These files are usually not compatible with UTF8!
+    parameter `path` is the absolute or relative path of the training data's root directory.
+    returns a list of strings.
+    """
 
-	count_vector = sklearn.feature_extraction.text.CountVectorizer()
-	files = sklearn.datasets.load_files(path)
-	num = []
-	for i in range(len(files.filenames)):
-		try:
-			count_vector.fit_transform(files.data[i:i+1])
-		except UnicodeDecodeError:
-			num.append(files.filenames[i])
-		except ValueError:
-			pass
+    count_vector = sklearn.feature_extraction.text.CountVectorizer()
+    files = sklearn.datasets.load_files(path)
+    num = []
+    for i in range(len(files.filenames)):
+        try:
+            count_vector.fit_transform(files.data[i:i + 1])
+        except UnicodeDecodeError:
+            num.append(files.filenames[i])
+        except ValueError:
+            pass
 
-	return num
+    return num
+
 
 def pretty_print_scores(scores):
-	"""
-	Prints mean and std of a list of scores, pretty and colorful!
-	parameter `scores` is a list of numbers.
-	"""
-	print colored ("                                      ", 'white', 'on_white')
-	print colored (" Mean accuracy: %0.3f (+/- %0.3f std) " % (scores.mean(), scores.std() / 2), 'magenta', 'on_white', attrs=['bold'])
-	print colored ("                                      ", 'white', 'on_white')
+    """
+    Prints mean and std of a list of scores, pretty and colorful!
+    parameter `scores` is a list of numbers.
+    """
+    print colored("                                      ", 'white', 'on_white')
+    print colored(" Mean accuracy: %0.3f (+/- %0.3f std) " % (scores.mean(), scores.std() / 2), 'magenta', 'on_white', attrs=['bold'])
+    print colored("                                      ", 'white', 'on_white')
+
 
 def cross_validation(data, target, classifier, cv=5):
-	"""
-	Does a cross validation with the classifier
-	parameters:
-		- `data`: array-like, shape=[n_samples, n_features]
-			Training vectors
-		- `target`: array-like, shape=[n_samples]
-			Target values for corresponding training vectors
-		- `classifier`: A classifier from the scikit-learn family would work!
-		- `cv`: number of times to do the cross validation. (default=5)
-	return a list of numbers, where the length of the list is equal to `cv` argument.
-	"""
-	return sklearn.cross_validation.cross_val_score(classifier, data, target, cv=cv)
+    """
+    Does a cross validation with the classifier
+    parameters:
+        - `data`: array-like, shape=[n_samples, n_features]
+            Training vectors
+        - `target`: array-like, shape=[n_samples]
+            Target values for corresponding training vectors
+        - `classifier`: A classifier from the scikit-learn family would work!
+        - `cv`: number of times to do the cross validation. (default=5)
+    return a list of numbers, where the length of the list is equal to `cv` argument.
+    """
+    return sklearn.cross_validation.cross_val_score(classifier, data, target, cv=cv)
+
 
 def bagOfWords(files_data):
-	"""
-	Converts a list of strings (which are loaded from files) to a BOW representation of it
-	parameter 'files_data' is a list of strings
-	returns a `scipy.sparse.coo_matrix` 
-	"""
+    """
+    Converts a list of strings (which are loaded from files) to a BOW representation of it
+    parameter 'files_data' is a list of strings
+    returns a `scipy.sparse.coo_matrix`
+    """
 
-	count_vector = sklearn.feature_extraction.text.CountVectorizer()
-	return count_vector.fit_transform(files_data)
+    count_vector = sklearn.feature_extraction.text.CountVectorizer()
+    return count_vector.fit_transform(files_data)
+
 
 def refine_all_emails(file_data):
-	"""
-	Does `refine_single_email` for every single email included in the list
-	parameter is a list of strings
-	returns NOTHING!
-	"""
+    """
+    Does `refine_single_email` for every single email included in the list
+    parameter is a list of strings
+    returns NOTHING!
+    """
 
-	for i, email in zip(range(len(file_data)),file_data):
-		file_data[i] = refine_single_email(email)
+    for i, email in zip(range(len(file_data)), file_data):
+        file_data[i] = refine_single_email(email)
 
-def refine_single_email(email):	
-	"""
-	Delete the unnecessary information in the header of emails
-	Deletes only lines in the email that starts with 'Path:', 'Newsgroups:', 'Xref:'
-	parameter is a string.
-	returns a string.
-	"""
 
-	parts = email.split('\n')
-	newparts = []
+def refine_single_email(email):
+    """
+    Delete the unnecessary information in the header of emails
+    Deletes only lines in the email that starts with 'Path:', 'Newsgroups:', 'Xref:'
+    parameter is a string.
+    returns a string.
+    """
 
-	# finished is when we have reached a line with something like 'Lines:' at the begining of it
-	# this is because we want to only remove stuff from headers of emails
-	# look at the dataset!
-	finished = False
-	for part in parts:
-		if finished:
-			newparts.append(part)
-			continue
-		if not (part.startswith('Path:') or part.startswith('Newsgroups:') or part.startswith('Xref:')) and not finished:
-			newparts.append(part)
-		if part.startswith('Lines:'):
-			finished = True
+    parts = email.split('\n')
+    newparts = []
 
-	return '\n'.join(newparts)
+    # finished is when we have reached a line with something like 'Lines:' at the begining of it
+    # this is because we want to only remove stuff from headers of emails
+    # look at the dataset!
+    finished = False
+    for part in parts:
+        if finished:
+            newparts.append(part)
+            continue
+        if not (part.startswith('Path:') or part.startswith('Newsgroups:') or part.startswith('Xref:')) and not finished:
+            newparts.append(part)
+        if part.startswith('Lines:'):
+            finished = True
+
+    return '\n'.join(newparts)
 
 if __name__ == '__main__':
-	main()
+    main()
